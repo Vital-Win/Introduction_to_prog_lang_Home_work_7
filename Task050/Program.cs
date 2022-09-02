c896826e40041c7c1d2fec2a62dd9aeffa834095
Vitaliy <vinogradovvitaliy2013@gmail.com>
2022-09-02 03:17:15 +0300

    Task050

---
+++ Task050/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 50. Напишите программу, которая на вход 
+// принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+
+void PrintArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] CreateArrayWithRandomNumbers()
+{
+    int[,] result = new int[3, 4];
+
+    var random = new Random();
+
+    for (var i = 0; i < result.GetLength(0); i++)
+    {
+        for (var j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = random.Next(1, 10);
+        }
+    }
+    return result;
+}
+
+
+Console.WriteLine("Введите позицию в строке (m)");
+if (!int.TryParse(Console.ReadLine()!, out var m))
+{
+    Console.WriteLine("Всё плохо");
+}
+
+Console.WriteLine("Введите позицию в столбце (n)");
+if (!int.TryParse(Console.ReadLine()!, out var n))
+{
+    Console.WriteLine("Всё плохо");
+}
+
+
+var array = CreateArrayWithRandomNumbers();
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+
+
+if (m < array.GetLength(0) && n < array.GetLength(1))
+    Console.WriteLine(array[m, n]);
+else
+    Console.WriteLine($"{m}{n} -> такого числа в массиве нет");
